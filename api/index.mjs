2353e942d99b5dcd8ac93c123a996bc8e1d64e8b
bokkyn <b.g.bokki@gmail.com>
2026-09-09 02:37:33 +0200

    Prepare backend

--- api/index.mjs
+++ api/index.mjs
@@ -1,9 +1,12 @@
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
 const { start } = require("../server/index.js");
 
 let appPromise;
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   appPromise ||= start({ listen: false });
   const app = await appPromise;
   return app(req, res);
-};
+}
